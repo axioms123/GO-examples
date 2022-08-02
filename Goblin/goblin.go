80899aa6f94a41334126f6e3f6b6f0737bfa8d83
axioms123 <61398302+axioms123@users.noreply.github.com>
2022-08-02 10:52:37 -0400

    update

--- Goblin/goblin.go
+++ Goblin/goblin.go
@@ -17,32 +17,101 @@
 //    After every 10 steps:
 //        The hero advances one level in the game.
 //        The hero may visit the "potion shop" to buy more potions for 4 gold each.
-//    When the hero dies, ask the player if they would like to play again.
-//        If the player says yes, create a new character with the same gold they had when they died.
-//        If the player says no, print out the level the current character reached with the number of goblins slain!
+//    When the hero dies, ask the protagonist if they would like to play again.
+//        If the protagonist says yes, create a new character with the same gold they had when they died.
+//        If the protagonist says no, print out the level the current character reached with the number of goblins slain!
 //
 package main
 
 import (
+	"math/rand"
 	"fmt"
-	"math/random"
-	"time"
 )
 
- type player struct{
-	health string
-	attack, defense, gold int16
-	potions []
-	go
+ type hero struct{
+	health, attack, defense, gold, NextStep int
+	potions []int
+}
 
-//constructor function
-func(std *Student) fill_defaults() {
-	//setting default values
-	//if no values present
-	if std.name == "" {
-		std.name = "character"
+type goblin struct {
+	health, attack, defense int
+}
+type game struct {
+	protagonist hero
+	antagonist goblin
+}
+
+
+func (g *game) createHero() {
+	g.protagonist = hero{
+		health:  rand.Intn(11) + 20,
+		attack:  rand.Intn(3) + 1,
+		defense: rand.Intn(5) + 1,
+		potions: []int{2, 2, 2, 2, 2},
+		gold:    0,
 	}
 }
+
+func createGoblin() goblin {
+	var newGoblin = goblin{
+		health:  rand.Intn(6) + 5,
+		attack:  rand.Intn(2) + 2,
+		defense: rand.Intn(2) + 1,
+	}
+	return newGoblin
+}
+
+
+func fight(protagonist *hero, goblin *goblin) bool {
+	for protagonist.health > 0 && goblin.health > 0 {
+		protagonistAttack := protagonist.attack
+		protagonistDefense := protagonist.defense
+		goblinAttack := goblin.attack
+		goblinDefense := goblin.defense
+
+
+		for protagonistDefense > 0 && goblinAttack > 0 {
+			protagonistDefense--
+			goblinAttack--
+		}
+		if goblinAttack > 0 {
+			for protagonist.health > 0 && goblinAttack > 0 {
+				protagonist.health--
+				goblinAttack--
+			}
+		}
+		for goblinDefense > 0 && protagonistAttack > 0 {
+			goblinDefense--
+			protagonistAttack--
+		}
+		if protagonistAttack > 0 {
+			for goblin.health > 0 && protagonistAttack > 0 {
+				goblin.health--
+				protagonistAttack--
+			}
+		}
+	}
+	//Notice if a protagonist won or lost 
+	if protagonist.health > 0 {
+		protagonist.gold += 2
+		return true 
+	} else {
+		return false 
+	}
+}
+
+
+func (h *hero) Step() {
+	h.NextStep++
+	var goblin goblin
+	goblinChance := rand.Intn(2)
+	if goblinChance == 1 {
+		goblin = createGoblin()
+		outcome := fight(h, &goblin)
+		return outcome;
+	}
+}
+
 func main() {
-	import 
+	fmt.Println("Goblin Tower Game")
 }
